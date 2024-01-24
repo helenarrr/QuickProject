0747aeee360543945dbbb76e6541d997f89ad60f
Elena Radchenko <elena.radchenko.9@mail.ru>
2024-01-24 23:54:28 +0300

    Adding a project structure

---
+++ backend/src/test/java/com/QuickProject/QuickProjectApp/QuickProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuickProject.QuickProjectApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickProjectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
