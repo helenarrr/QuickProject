82e81d267845b4c993a45a22c2b32a2fcad96551
Elena Radchenko <elena.radchenko.9@mail.ru>
2024-03-05 00:13:03 +0300

    started work with filter

---
+++ backend/src/main/java/com/QuickProject/QuickProjectApp/config/JwtAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.QuickProject.QuickProjectApp.config;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(@Nonnull HttpServletRequest request,
+                                    @Nonnull HttpServletResponse response,
+                                    @Nonnull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
+}
---
+++ backend/src/main/java/com/QuickProject/QuickProjectApp/config/JwtService.java
@@ -0,0 +1,12 @@
+package com.QuickProject.QuickProjectApp.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+
+        return null;
+
+    }
+}
